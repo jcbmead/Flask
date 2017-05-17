1763429b89f9717ebf886351ccfffc8d4cbc3bf8
jcbmead <josephcb.mead@gmail.com>
2017-05-17 14:11:35 +0100

    Custom error page not found

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ bootstrap = Bootstrap(app)
 def index():
 	return render_template('index.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
+
 if __name__ == '__main__':
 	app.config['DEBUG'] = True
 	app.run(host='0.0.0.0')
